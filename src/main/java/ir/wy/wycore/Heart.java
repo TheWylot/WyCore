829143dee7bd018a788baf2c7613c19fbef03c88
TheWylot <thewylot22@gmail.com>
2022-01-10 22:00:38 +0330

    Item Handler & xml Changes

--- src/main/java/ir/wy/wycore/Heart.java
+++ src/main/java/ir/wy/wycore/Heart.java
@@ -126,6 +126,5 @@ public class Heart {
         public JsonFactory getFactory() {
             return factory;
         }
-
     }
 }
---
+++ src/main/java/ir/wy/wycore/Item.java
@@ -0,0 +1,57 @@
+package ir.wy.wycore;
+
+import com.cryptomorin.xseries.XMaterial;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@NoArgsConstructor
+public class Item {
+    public String headOwner;
+    public String headData;
+    public String displayName;
+    public List<String> lore;
+    public XMaterial material;
+    public Integer slot;
+    public int amount;
+
+    public Item(XMaterial material, int amount, String displayName, List<String> lore) {
+        this.material = material;
+        this.amount = amount;
+        this.lore = lore;
+        this.displayName = displayName;
+    }
+
+    public Item(XMaterial material, int slot, int amount, String displayName, List<String> lore) {
+        this.material = material;
+        this.amount = amount;
+        this.lore = lore;
+        this.displayName = displayName;
+        this.slot = slot;
+    }
+
+    public Item(XMaterial material, int slot, String headData, int amount, String displayName, List<String> lore) {
+        this.material = material;
+        this.amount = amount;
+        this.lore = lore;
+        this.displayName = displayName;
+        this.slot = slot;
+        this.headData = headData;
+    }
+
+    public Item(XMaterial material, int slot, int amount, String displayName, String headOwner, List<String> lore) {
+        this.material = material;
+        this.amount = amount;
+        this.lore = lore;
+        this.displayName = displayName;
+        this.headOwner = headOwner;
+        this.slot = slot;
+    }
+
+    public Item(XMaterial material, int amount, String displayName, String headOwner, List<String> lore) {
+        this.material = material;
+        this.amount = amount;
+        this.lore = lore;
+        this.displayName = displayName;
+        this.headOwner = headOwner;
+    }
+}
