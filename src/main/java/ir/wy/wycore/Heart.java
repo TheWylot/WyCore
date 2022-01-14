30ef78d60dc284274d6489bb9a01ef4f5618e59a
TheWylot <thewylot22@gmail.com>
2022-01-14 18:23:13 +0330

    Fix Errors

--- src/main/java/ir/wy/wycore/Heart.java
+++ src/main/java/ir/wy/wycore/Heart.java
@@ -14,17 +14,24 @@ import java.lang.reflect.Type;
 
 public class Heart {
 
-
     private final ObjectMapper objectMapper;
+    private final HeartType heartType;
     private final JavaPlugin javaPlugin;
 
-
     public Heart(HeartType heartType, JavaPlugin javaPlugin) {
+        this.heartType = heartType;
         this.javaPlugin = javaPlugin;
 
-        objectMapper = new ObjectMapper(heartType.getFactory()).configure(JsonParser.Feature.IGNORE_UNDEFINED, true);
+        objectMapper = new ObjectMapper(heartType.getFactory()).configure(JsonParser.Feature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER, true);
     }
 
+    /**
+     * The name of a provided class.
+     * Used as the file name for persisting.
+     *
+     * @param clazz The class whose name is to be returned
+     * @return The name of the configuration file
+     */
     private static String getName(Class<?> clazz) {
         return clazz.getSimpleName().toLowerCase();
     }
@@ -37,8 +44,11 @@ public class Heart {
         return getName(type.getClass());
     }
 
+    // ------------------------------------------------------------ //
+    // GET FILE - In which file would we like to store this object?
+    // ------------------------------------------------------------ //
     public File getFile(String name) {
-        return new File(javaPlugin.getDataFolder(), name + HeartType.getExtension());
+        return new File(javaPlugin.getDataFolder(), name + heartType.getExtension());
     }
 
     public File getFile(Class<?> clazz) {
@@ -50,7 +60,6 @@ public class Heart {
     }
 
     // SAVE
-
     public void save(Object instance) {
         save(instance, getFile(instance));
     }
@@ -74,6 +83,7 @@ public class Heart {
         return "";
     }
 
+    // LOAD BY CLASS
     public <T> T load(Class<T> clazz) {
         return load(clazz, getFile(clazz));
     }
@@ -95,6 +105,15 @@ public class Heart {
         return null;
     }
 
+    /**
+     * Creates a new instance of a class and deserializes the provided content.
+     * Returns the new instance.
+     *
+     * @param clazz   The class which should be loaded
+     * @param content The content which should be loaded into the class
+     * @param <T>     The type of class
+     * @return The loaded class. Might be null
+     */
     public <T> T load(Class<T> clazz, String content) {
         try {
             return objectMapper.readValue(content, clazz);
@@ -106,6 +125,10 @@ public class Heart {
         return null;
     }
 
+    /**
+     * Represents a data format which can be
+     * used for persisting.
+     */
     public enum HeartType {
 
         YAML(".yml", new YAMLFactory()),
@@ -114,6 +137,12 @@ public class Heart {
         private final String extension;
         private final JsonFactory factory;
 
+        /**
+         * The default constructor.
+         *
+         * @param extension The file extension with a leading full stop
+         * @param factory   The JsonFactory which should be used for persisting
+         */
         HeartType(String extension, JsonFactory factory) {
             this.extension = extension;
             this.factory = factory;
@@ -126,5 +155,6 @@ public class Heart {
         public JsonFactory getFactory() {
             return factory;
         }
+
     }
 }
