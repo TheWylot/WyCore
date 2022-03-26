fb5b95613366016c32626e76a4e0f9638927cc05
TheWylot <thewylot22@gmail.com>
2022-03-26 19:41:30 +0430

    Item.java - Adding headOwnerUUID

--- src/main/java/ir/wy/wycore/Item.java
+++ src/main/java/ir/wy/wycore/Item.java
@@ -3,6 +3,7 @@ package ir.wy.wycore;
 import com.cryptomorin.xseries.XMaterial;
 import lombok.NoArgsConstructor;
 import java.util.List;
+import java.util.UUID;
 
 @NoArgsConstructor
 public class Item {
@@ -11,6 +12,7 @@ public class Item {
     public String displayName;
     public List<String> lore;
     public XMaterial material;
+    public UUID headOwnerUUID;
     public Integer slot;
     public int amount;
 
@@ -54,4 +56,14 @@ public class Item {
         this.displayName = displayName;
         this.headOwner = headOwner;
     }
+
+    public Item(XMaterial material, int amount, String displayName, String headOwner, UUID ownerUUID, List<String> lore) {
+        this.material = material;
+        this.amount = amount;
+        this.lore = lore;
+        this.displayName = displayName;
+        this.headOwnerUUID = ownerUUID;
+        this.headOwner = headOwner;
+    }
+
 }
