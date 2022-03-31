b7b485688942f7b8bc24fbcf9909738afd5d1c08
TheWylot <thewylot22@gmail.com>
2022-03-31 13:30:05 +0430

    v0.6 POM XML Update | Paged GUI Updates

--- src/main/java/ir/wy/wycore/gui/PagedGUI.java
+++ src/main/java/ir/wy/wycore/gui/PagedGUI.java
@@ -7,10 +7,13 @@ import ir.wy.wycore.utils.ItemStackUtils;
 
 
 import lombok.AllArgsConstructor;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -19,30 +22,69 @@ import java.util.stream.Collectors;
 public abstract class PagedGUI<T> implements GUI {
 
 
-    private final int page;
+    private int page;
+    private final int size;
     private final Design design;
     private final Item previousPage;
     private final Item nextPage;
+    private final Map<Integer, T> items = new HashMap<>();
 
     @Override
     public void addContent(Inventory inventory) {
+        items.clear();
         InventoryUtils.fillInventory(inventory, design);
 
-        inventory.setItem(inventory.getSize() - 3, ItemStackUtils.makeItem(nextPage));
-        inventory.setItem(inventory.getSize() - 7, ItemStackUtils.makeItem(previousPage));
+        if (isPaged()) {
+            inventory.setItem(inventory.getSize() - 3, ItemStackUtils.makeItem(nextPage));
+            inventory.setItem(inventory.getSize() - 7, ItemStackUtils.makeItem(previousPage));
+        }
 
-        int elementsPerPage = inventory.getSize() - 9;
+        int elementsPerPage = inventory.getSize() - (isPaged() ? 9 : 0);
         List<T> objects = getPageObjects().stream()
                 .skip((long) (page - 1) * elementsPerPage)
                 .limit(elementsPerPage)
                 .collect(Collectors.toList());
         AtomicInteger slot = new AtomicInteger(0);
         for (T t : objects) {
-            inventory.setItem(slot.getAndIncrement(), getItemStack(t));
+            int currentSlot = slot.getAndIncrement();
+            items.put(currentSlot, t);
+            inventory.setItem(currentSlot, getItemStack(t));
         }
     }
 
     public abstract Collection<T> getPageObjects();
 
     public abstract ItemStack getItemStack(T t);
+
+    public T getItem(int slot) {
+        return items.get(slot);
+    }
+
+    public int getSize() {
+        int newSize = size;
+        if (size <= 0) {
+            newSize = (int) (Math.ceil(getPageObjects().size() / 9.0) * 9);
+        }
+        return Math.max(Math.min(newSize, 54), 9);
+    }
+
+    public boolean isPaged() {
+        return getPageObjects().size() > getSize();
+    }
+
+    @Override
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (!isPaged()) return;
+        if (event.getSlot() == getInventory().getSize() - 7) {
+            if (page > 1) {
+                page--;
+                event.getWhoClicked().openInventory(getInventory());
+            }
+        } else if (event.getSlot() == getInventory().getSize() - 3) {
+            if ((event.getInventory().getSize() - 9) * page < getPageObjects().size()) {
+                page++;
+                event.getWhoClicked().openInventory(getInventory());
+            }
+        }
+    }
 }
