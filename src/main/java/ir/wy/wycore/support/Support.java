8087108155d753655dd855fcca1af7dbe093f272
Soheil <TheWylot22@gmail.com>
2022-12-10 14:48:39 +0330

    v2.0.0 | Core AntiCheat Support

---
+++ src/main/java/ir/wy/wycore/support/Support.java
@@ -0,0 +1,5 @@
+package ir.wy.wycore.support;
+
+public interface Support {
+    String getPluginName();
+}
---
+++ src/main/java/ir/wy/wycore/support/SupportManager.kt
@@ -0,0 +1,29 @@
+package ir.wy.wycore.support
+
+import org.bukkit.Bukkit
+import org.bukkit.plugin.Plugin
+import java.util.*
+import java.util.stream.Collectors
+
+class SupportManager(
+    val pluginName: String,
+    private val runnable: Runnable
+) {
+
+    fun loadIfPresent() {
+        if (LOADED_PLUGINS.contains(pluginName.lowercase(Locale.getDefault()))) {
+            load()
+        }
+    }
+
+    fun load() {
+        runnable.run()
+    }
+
+    companion object {
+        private val LOADED_PLUGINS = Arrays.stream(Bukkit.getPluginManager().plugins)
+            .map { obj: Plugin -> obj.name }
+            .map { obj: String -> obj.lowercase(Locale.getDefault()) }
+            .collect(Collectors.toSet())
+    }
+}
