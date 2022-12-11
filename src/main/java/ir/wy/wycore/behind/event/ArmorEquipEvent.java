0196ca1b54bae781dfdf88480fb1c0e7e0d06e43
Soheil <TheWylot22@gmail.com>
2022-12-11 14:46:10 +0330

    v2.0.0 | Armor Change/Equip Event

---
+++ src/main/java/ir/wy/wycore/behind/event/ArmorEquipEvent.java
@@ -0,0 +1,25 @@
+package ir.wy.wycore.behind.event;
+
+import com.Zrips.CMI.Modules.BungeeCord.ServerListPing;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class ArmorEquipEvent extends PlayerEvent {
+
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    public ArmorEquipEvent(@NotNull final Player player) {
+        super(player);
+    }
+
+    @Override @NotNull
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+}
