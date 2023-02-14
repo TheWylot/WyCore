7608c2cb0d5f6eee472d675858831f111b32b4f4
Soheil <TheWylot22@gmail.com>
2023-02-14 14:57:41 +0330

    DEV-2.0.4 // Added Intave AntiCheat Support

--- src/main/java/ir/wy/wycore/spigot/support/anticheat/IntaveAntiCheat.kt
+++ src/main/java/ir/wy/wycore/spigot/support/anticheat/IntaveAntiCheat.kt
@@ -1,4 +1,33 @@
 package ir.wy.wycore.spigot.support.anticheat
 
-class IntaveAntiCheat {
+import de.jpx3.intave.access.check.event.IntaveViolationEvent
+import ir.wy.wycore.behind.support.anticheat.AntiCheatSupport
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
+import java.util.UUID
+
+class IntaveAntiCheat : AntiCheatSupport, Listener{
+    private val exempt: MutableSet<UUID> = HashSet()
+
+    override fun getPluginName(): String {
+        return "Intave"
+    }
+
+    override fun exempt(player: Player) {
+        exempt.add(player.uniqueId)
+    }
+
+    override fun unexempt(player: Player) {
+        exempt.remove(player.uniqueId)
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    private fun onViolate(event: IntaveViolationEvent) {
+        if(!exempt.contains(event.player().uniqueId)) {
+            return
+        }
+        event.reaction().equals(true)
+    }
 }
