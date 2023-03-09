fa12763c4e57ff44d3426f8c60f6195c243fe50e
Soheil <TheWylot22@gmail.com>
2023-03-09 12:42:48 +0330

    DEV-2.0.17 // Fixed Polar AntiCheat Support

--- src/main/java/ir/wy/wycore/spigot/support/anticheat/PolarAntiCheat.kt
+++ src/main/java/ir/wy/wycore/spigot/support/anticheat/PolarAntiCheat.kt
@@ -1,4 +1,23 @@
 package ir.wy.wycore.spigot.support.anticheat
 
-class PolarAntiCheat {
+import ir.wy.wycore.behind.support.anticheat.AntiCheatSupport
+import org.bukkit.entity.Player
+import org.bukkit.event.Listener
+import java.util.*
+import kotlin.collections.HashSet
+
+class PolarAntiCheat : AntiCheatSupport, Listener {
+    private val exempt: MutableSet<UUID> = HashSet();
+
+    override fun getPluginName(): String {
+        return "Polar"
+    }
+
+    override fun exempt(player: Player) {
+        exempt.add(player.uniqueId)
+    }
+
+    override fun unexempt(player: Player) {
+        exempt.remove(player.uniqueId)
+    }
 }
