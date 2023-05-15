31abed7096f1d7f6b0cf352ed548e2615529586b
Soheil <TheWylot22@gmail.com>
2023-05-15 18:29:14 +0330

    DEV-2.0.25 // Hologram/Entity Support Improvements

--- src/main/java/ir/wy/wycore/behind/support/builtentity/BuiltEntityManager.java
+++ src/main/java/ir/wy/wycore/behind/support/builtentity/BuiltEntityManager.java
@@ -1,21 +1,23 @@
 package ir.wy.wycore.behind.support.builtentity;
 
+import ir.wy.wycore.spigot.support.SupportRegistery;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashSet;
 import java.util.Set;
-public class BuiltEntityManager {
+public final class BuiltEntityManager {
 
+    private static final SupportRegistery<BuiltEntitySupport> REGISTERY = new SupportRegistery<>();
+
+
+    @Deprecated
     private static final Set<BuiltEntitySupport> REGISTERED = new HashSet<>();
 
     public static void register(@NotNull final BuiltEntitySupport Support) {
-        REGISTERED.removeIf(it -> it.getPluginName().equalsIgnoreCase(Support.getPluginName()));
-        REGISTERED.add(Support);
+        REGISTERY.register(Support);
     }
 
     public static void registerAllEntities() {
-        for (BuiltEntitySupport integration : REGISTERED) {
-            integration.registerAllEntities();
-        }
+        REGISTERY.forEachSafely(BuiltEntitySupport::registerAllEntities);
     }
 }
